0c9be377818ac0960d9228f3a38fee24e9552e14
Sila Setthakan-anan <sila.s@lmginsurance.com>
2025-04-03 23:28:39 +0700

    1st commit - setup mono repos project.

---
+++ services/rest/reactive-api/src/main/java/dev/fresult/service/ReactiveApiApplication.java
@@ -0,0 +1,25 @@
+package dev.fresult.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@SpringBootApplication
+public class ReactiveApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ReactiveApiApplication.class, args);
+  }
+
+}
+
+@RestController
+class ReactiveController {
+  @GetMapping("/hello")
+  public Mono<ResponseEntity<String>> hello() {
+    return Mono.just(ResponseEntity.ok("Hello"));
+  }
+}
